25dcec5e482e855c899bae89723e99df8cc32401
Farhad Ahmed <toons0animotica@gmail.com>
2025-12-27 17:21:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ async def watch(episode_id: str, server: str = "HD-1", type: str = "sub"):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=25565)
